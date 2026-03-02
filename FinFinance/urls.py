61a29219b0ead910c224852a60aca0b7be87f660
mariadeniston <mariadeniston111@gmail.com>
2026-03-02 22:36:25 +0530

    urls static removed

--- FinFinance/urls.py
+++ FinFinance/urls.py
@@ -77,5 +77,5 @@ urlpatterns = [
     path('notifications/', views.notifications, name='notifications'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
